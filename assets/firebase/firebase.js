12bf11357020a9404befc9494678958b3ac1b00e
alomporamanik687-dotcom <alomporamanik687@gmail.com>
2026-05-23 15:47:32 +0600

    firebase.js

--- assets/firebase/firebase.js
+++ assets/firebase/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbcniCvz17GdiGldDw_038fQ5qIzWnM8Y",
-  authDomain: "earnpro-ac803.firebaseapp.com",
-  projectId: "earnpro-ac803",
-  storageBucket: "earnpro-ac803.firebasestorage.app",
-  messagingSenderId: "217418853905",
-  appId: "1:217418853905:web:70dc1445b14232743c0bb0"
+  apiKey: "AIzaSyDQHOaLwHrdWFhyaUCKyid3A526_VjCfVg",
+  authDomain: "earn-pro-d69f1.firebaseapp.com",
+  projectId: "earn-pro-d69f1",
+  storageBucket: "earn-pro-d69f1.firebasestorage.app",
+  messagingSenderId: "197193236556",
+  appId: "1:197193236556:web:5dc40d97562dbfe9baf4e9",
+  measurementId: "G-C41GFLX6ZM"
 };
 
 const app = initializeApp(firebaseConfig);
